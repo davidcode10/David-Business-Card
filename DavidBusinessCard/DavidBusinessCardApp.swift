6ded2f457fdba7e62b3a6e4874321d26235e98e4
davidcode10 <davidedson2030@outlook.com>
2022-07-29 18:27:36 +0200

    Initial Commit

---
+++ DavidBusinessCard/DavidBusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DavidBusinessCardApp.swift
+//  DavidBusinessCard
+//
+//  Created by David Santiago on 29/7/22.
+//
+
+import SwiftUI
+
+@main
+struct DavidBusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DavidBusinessCard/InfoView.swift
@@ -0,0 +1,32 @@
+//
+//  InfoView.swift
+//  DavidBusinessCard
+//
+//  Created by David Santiago on 29/7/22.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 50)
+            .fill(Color.white)
+            .frame(height: 50.0)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text)
+            })
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "+34 696 72 19 16", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
